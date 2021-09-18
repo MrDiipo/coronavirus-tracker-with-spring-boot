4ab37c53a9c2914a1735a4506389cf7c3407c6e0
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-18 16:39:38 +0100

    Saving LocationStats implementations.

--- coronavirus-tracker/src/main/java/com/mrdiipo/coronavirustracker/services/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/com/mrdiipo/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package com.mrdiipo.coronavirustracker.services;
 
+import com.mrdiipo.coronavirustracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,15 +15,21 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
+
+        List<LocationStats> newStats = new ArrayList<>();
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -36,12 +43,20 @@ public class CoronaVirusDataService {
         StringReader csvBodyReader = new StringReader(httpResponse.body());
 
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
-        for (CSVRecord record:
-             records) {
-            String state = record.get("Province/State");
+
+        for (CSVRecord record: records) {
+
+            LocationStats locationStats = new LocationStats();
+            locationStats.setState(record.get("Province/State"));
+            locationStats.setCountry(record.get("Country/Region"));
+//            String state = record.get("Province/State");
 //            String customerNo = record.get("");
 //            String name = record.get("Name");
-            System.out.println(state);
+//            System.out.println(state);
+            locationStats.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(locationStats);
+            newStats.add(locationStats);
         }
+        this.allStats = newStats;
     }
 }
