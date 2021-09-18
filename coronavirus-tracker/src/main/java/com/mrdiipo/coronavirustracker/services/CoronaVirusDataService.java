f0ce2a1655494748bed268994bd3ea32efa25cb2
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-18 16:20:39 +0100

    Enabling AutoScheduling.

--- coronavirus-tracker/src/main/java/com/mrdiipo/coronavirustracker/services/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/com/mrdiipo/coronavirustracker/services/CoronaVirusDataService.java
@@ -2,12 +2,14 @@ package com.mrdiipo.coronavirustracker.services;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -19,6 +21,7 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
+    @Scheduled(cron = "* * * * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
@@ -28,14 +31,17 @@ public class CoronaVirusDataService {
 
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(httpResponse.body());
+//        System.out.println(httpResponse.body());
 
-        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record:
              records) {
-            String id = record.get("ID");
-            String customerNo = record.get("CustomerNo");
-            String name = record.get("Name");
+            String state = record.get("Province/State");
+//            String customerNo = record.get("");
+//            String name = record.get("Name");
+            System.out.println(state);
         }
     }
 }
