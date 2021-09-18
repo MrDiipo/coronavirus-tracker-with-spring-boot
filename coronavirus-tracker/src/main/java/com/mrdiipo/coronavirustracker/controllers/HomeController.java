7601b28b88203bf85f2024d5381b2f1e57bf4645
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-18 17:12:14 +0100

    Thymeleaf completion.

--- coronavirus-tracker/src/main/java/com/mrdiipo/coronavirustracker/controllers/HomeController.java
+++ coronavirus-tracker/src/main/java/com/mrdiipo/coronavirustracker/controllers/HomeController.java
@@ -4,6 +4,7 @@ import com.mrdiipo.coronavirustracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
@@ -11,6 +12,7 @@ public class HomeController {
     @Autowired
     CoronaVirusDataService coronaVirusDataService;
 
+    @GetMapping("/")
     public String home(Model model){
         model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
         return "home";
